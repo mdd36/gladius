f129ef775b6cedd229726e4d3a64549dd45ae137
Matt Dickson <matthew.dickson.137@gmail.com>
2023-09-22 14:59:03 -0700

    Add basic test for hashing

--- core/src/position/hash.rs
+++ core/src/position/hash.rs
@@ -1,7 +1,7 @@
 use rand::{rngs::SmallRng, RngCore, SeedableRng};
 
 use super::{
-	board::{CASTLE_RIGHTS_SQUARES, ROOKS},
+	board::CASTLE_RIGHTS_SQUARES,
 	moves::{Move, MOVE_DIRECTION},
 	CastleSide, Color, Piece, Position,
 };
@@ -21,9 +21,9 @@ const ZOBRIST_RANDOM_SEED: u64 = 0x34e1a1e8655c3200;
 // After all the piece moves -- 2 sides, six pieces each, 64 squares on the board.
 const TO_MOVE_ZOBRIST_INDEX: usize = 2 * 6 * 64;
 // Castling values start right after the to move value.
-const CASTLING_ZOBIRST_INDEX: usize = TO_MOVE_ZOBRIST_INDEX + 1;
+const CASTLING_ZOBRIST_INDEX: usize = TO_MOVE_ZOBRIST_INDEX + 1;
 // En passant values start after the 4 castling values.
-const EN_PASSANT_ZOBRIST_INDEX: usize = CASTLING_ZOBIRST_INDEX + 4;
+const EN_PASSANT_ZOBRIST_INDEX: usize = CASTLING_ZOBRIST_INDEX + 4;
 
 fn init_zobrist() -> [u64; 781] {
 	let mut random = SmallRng::seed_from_u64(ZOBRIST_RANDOM_SEED);
@@ -34,6 +34,11 @@ fn init_zobrist() -> [u64; 781] {
 	zobrist_randoms
 }
 
+/// Hash a position. This is inherently lossy and collision will occur --
+/// the total number of chess positions is on the order of 10^46, whereas
+/// the number of unique 64 bit values is on the order of 10^19. There
+/// isn't much we can do to avoid that, and it's a tradeoff that we need to
+/// accept if we want the benefits of a position cache (and we do).
 pub fn hash(position: &Position) -> u64 {
 	let mut hash = 0;
 
@@ -45,7 +50,7 @@ pub fn hash(position: &Position) -> u64 {
 			for square in (piece_locations & color_board) {
 				let zobirst_value_index = (color as usize) * (64 * 6) + // Select the right color range
 					(piece as usize - 2) * 64 + // Select the right piece range
-					square.as_usize(); // Select the square that the piece is on
+					square.lsb_index(); // Select the square that the piece is on
 
 				hash ^= ZOBRIST_RANDOMS[zobirst_value_index];
 			}
@@ -62,28 +67,28 @@ pub fn hash(position: &Position) -> u64 {
 		.metadata
 		.can_castle(Color::White, super::CastleSide::Queen)
 	{
-		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX];
+		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX];
 	}
 
 	if position
 		.metadata
 		.can_castle(Color::White, super::CastleSide::King)
 	{
-		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + 1];
+		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + 1];
 	}
 
 	if position
 		.metadata
 		.can_castle(Color::Black, super::CastleSide::Queen)
 	{
-		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + 2];
+		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + 2];
 	}
 
 	if position
 		.metadata
 		.can_castle(Color::Black, super::CastleSide::King)
 	{
-		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + 3];
+		hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + 3];
 	}
 	// Hash component from en passant
 	if let Some(square) = position.metadata.en_passant_square() {
@@ -93,33 +98,35 @@ pub fn hash(position: &Position) -> u64 {
 	hash
 }
 
-pub fn increment_hash(old: u64, starting_position: &Position, to_apply: &Move) -> u64 {
+/// Calculating the original hash is expensive, so we want to avoid doing it as much as
+/// possible. This function will produce the hash value of a position after a given move,
+/// assuming that it's given the hash of the starting position.
+pub fn hash_after_move(old: u64, starting_position: &Position, to_apply: &Move) -> u64 {
 	let mut hash = old;
 	let piece = starting_position.piece_on(to_apply.start).unwrap();
 	let to_move = starting_position.metadata.to_move();
 
 	// Update the hash to reflect the moved piece.
 	let origin_square_index =
-		(to_move as usize) * (6 * 64) + (piece as usize - 2) * 64 + to_apply.start.as_usize();
+		(to_move as usize) * (6 * 64) + (piece as usize - 2) * 64 + to_apply.start.lsb_index();
 	hash ^= ZOBRIST_RANDOMS[origin_square_index];
 
 	let destination_square_index =
-		(to_move as usize) * (6 * 64) + (piece as usize - 2) * 64 + to_apply.target.as_usize();
+		(to_move as usize) * (6 * 64) + (piece as usize - 2) * 64 + to_apply.target.lsb_index();
 	hash ^= ZOBRIST_RANDOMS[destination_square_index];
 
 	// Update the hash to remove a captured piece
 	if to_apply.flags.is_en_passant() {
-		let captured_piece = starting_position.piece_on(to_apply.target).unwrap();
 		let capture_square = to_apply.target >> (8 * MOVE_DIRECTION[to_move as usize]);
 		let captured_piece_square_index = (1 - to_move as usize) * (6 * 64)
 			+ (Piece::Pawn as usize - 2) * 64
-			+ capture_square.as_usize();
+			+ capture_square.lsb_index();
 		hash ^= ZOBRIST_RANDOMS[captured_piece_square_index];
 	} else if to_apply.flags.is_capture() {
 		let captured_piece = starting_position.piece_on(to_apply.target).unwrap();
 		let captured_piece_square_index = (1 - to_move as usize) * (6 * 64)
 			+ (captured_piece as usize - 2) * 64
-			+ to_apply.target.as_usize();
+			+ to_apply.target.lsb_index();
 		hash ^= ZOBRIST_RANDOMS[captured_piece_square_index];
 	}
 
@@ -129,22 +136,19 @@ pub fn increment_hash(old: u64, starting_position: &Position, to_apply: &Move) -
 	// Update castling rights
 	if piece == Piece::King {
 		if starting_position.can_castle(to_move, CastleSide::King) {
-			hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + to_move as usize];
+			hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + to_move as usize];
 		}
 
 		if starting_position.can_castle(to_move, CastleSide::Queen) {
-			hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + to_move as usize + 1];
+			hash ^= ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + to_move as usize + 1];
 		}
 	}
 
-	if piece == Piece::Rook {
-		for side in [CastleSide::King, CastleSide::Queen] {
-			let starting_rook_square = ROOKS[side as usize][to_move as usize].as_u64();
-			let is_from_starting_position = to_apply.start & starting_rook_square != 0;
-			if is_from_starting_position && starting_position.can_castle(to_move, side) {
-				hash ^= ZOBRIST_RANDOMS
-					[CASTLING_ZOBIRST_INDEX + (2 * to_move as usize) + side as usize];
-			}
+	if piece == Piece::Rook && (CASTLE_RIGHTS_SQUARES & to_apply.start).has_pieces() {
+		let side = CastleSide::from(to_apply.start);
+		if starting_position.can_castle(to_move, side) {
+			hash ^=
+				ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + (2 * to_move as usize) + side as usize];
 		}
 	}
 
@@ -152,15 +156,72 @@ pub fn increment_hash(old: u64, starting_position: &Position, to_apply: &Move) -
 		let side = CastleSide::from(to_apply.target);
 		if starting_position.can_castle(!to_move, side) {
 			hash ^=
-				ZOBRIST_RANDOMS[CASTLING_ZOBIRST_INDEX + (2 * !to_move as usize) + side as usize];
+				ZOBRIST_RANDOMS[CASTLING_ZOBRIST_INDEX + (2 * !to_move as usize) + side as usize];
 		}
 	}
 
 	// Update the hash from en passant
+	if let Some(square) = starting_position.metadata.en_passant_square() {
+		let en_passant_file = square.file() as usize;
+		hash ^= ZOBRIST_RANDOMS[EN_PASSANT_ZOBRIST_INDEX + en_passant_file];
+	}
+
 	if to_apply.flags.is_double_pawn_push() {
 		let en_passant_file = to_apply.target.file() as usize;
 		hash ^= ZOBRIST_RANDOMS[EN_PASSANT_ZOBRIST_INDEX + en_passant_file];
 	}
 
 	hash
+}
+
+#[cfg(test)]
+mod test {
+
+	use super::*;
+
+	#[test]
+	pub fn test_hash_update() {
+		let mut position = Position::default();
+		let mut running_hash = hash(&position);
+
+		// En passant created
+		let m = Move::from_uci_str("e2e4", &position);
+		running_hash = hash_after_move(running_hash, &position, &m);
+		position = position.apply_move(&m);
+		let expected_hash = hash(
+			&Position::from_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+				.unwrap(),
+		);
+		assert_eq!(expected_hash, running_hash);
+
+		// En passant cleared
+		let m = Move::from_uci_str("g8f6", &position);
+		running_hash = hash_after_move(running_hash, &position, &m);
+		position = position.apply_move(&m);
+		let expected_hash = hash(
+			&Position::from_fen("rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")
+				.unwrap(),
+		);
+		assert_eq!(expected_hash, running_hash);
+
+		// Castling rights are gone for white
+		let m = Move::from_uci_str("e1e2", &position);
+		running_hash = hash_after_move(running_hash, &position, &m);
+		position = position.apply_move(&m);
+		let expected_hash = hash(
+			&Position::from_fen("rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPPKPPP/RNBQ1BNR b kq - 0 1")
+				.unwrap(),
+		);
+		assert_eq!(expected_hash, running_hash);
+
+		// Captured a piece
+		let m = Move::from_uci_str("f6e4", &position);
+		running_hash = hash_after_move(running_hash, &position, &m);
+		position = position.apply_move(&m);
+		let expected_hash = hash(
+			&Position::from_fen("rnbqkb1r/pppppppp/8/8/4n3/8/PPPPKPPP/RNBQ1BNR w kq - 0 1")
+				.unwrap(),
+		);
+		assert_eq!(expected_hash, running_hash);
+	}
 }
